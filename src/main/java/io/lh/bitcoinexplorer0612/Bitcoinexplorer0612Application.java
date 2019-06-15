3f849769376de364a2c626528f02d77c20b44bfa
lihong <1741629633@qq.com>
2019-06-15 08:51:41 +0800

    add mybatis files

--- src/main/java/io/lh/bitcoinexplorer0612/Bitcoinexplorer0612Application.java
+++ src/main/java/io/lh/bitcoinexplorer0612/Bitcoinexplorer0612Application.java
@@ -1,9 +1,11 @@
 package io.lh.bitcoinexplorer0612;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.lh.bitcoinexplorer0612.dao")
 public class Bitcoinexplorer0612Application {
 
     public static void main(String[] args) {
