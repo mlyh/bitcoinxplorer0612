2f11ff96bae5b516186034a76c41ed4934f92ab4
lihong <1741629633@qq.com>
2019-06-17 09:02:23 +0800

    add rest api3

--- src/main/java/io/lh/bitcoinexplorer0612/controller/TempController.java
+++ src/main/java/io/lh/bitcoinexplorer0612/controller/TempController.java
@@ -17,16 +17,16 @@ public class TempController {
 
     @GetMapping("/test")
     public String test(){
-       // JSONObject blockChainIndo = bitcoinRestApi.getBlockChainIndo();
-        //JSONObject blockNoDetails = bitcoinRestApi.getBlockNoDetails("00000000000008dba07e7d40622786b2bebc6118ce3c8188e47a01139691d121");
-        //JSONObject block = bitcoinRestApi.getBlock("00000000000008dba07e7d40622786b2bebc6118ce3c8188e47a01139691d121");
-        //JSONObject headers = bitcoinRestApi.getBlockHeaders(5,"000000000000003b90c4e88fdf15c1394ab945f41978cafa445d8ee1ef63525b");
+       // JSONObject b = bitcoinRestApi.getBlockChainIndo();
+        //JSONObject b = bitcoinRestApi.getBlockNoDetails("00000000000008dba07e7d40622786b2bebc6118ce3c8188e47a01139691d121");
+        //JSONObject b = bitcoinRestApi.getBlock("00000000000008dba07e7d40622786b2bebc6118ce3c8188e47a01139691d121");
+        //JSONObject b = bitcoinRestApi.getBlockHeaders(5,"00000000003751468e16e359fade039e0798c8bc9a830896ed20854840e10fed");
 
         //JSONObject b = bitcoinRestApi.getTransactions("000000000000003b90c4e88fdf15c1394ab945f41978cafa445d8ee1ef63525b");
-        //JSONObject b = bitcoinRestApi.getBlockhashByHeight("000000000000003b90c4e88fdf15c1394ab945f41978cafa445d8ee1ef63525b");
+        //JSONObject b = bitcoinRestApi.getBlockhashByHeight("148106");
         //JSONObject b = bitcoinRestApi.getMempoolInfo("000000000000003b90c4e88fdf15c1394ab945f41978cafa445d8ee1ef63525b");
         JSONObject b = bitcoinRestApi.getMempoolContents();
-        return b.toJSONString();
+        return JSONObject.toJSONString(b);
     }
 
 
